841a4735d3acff3523fbd75c450301d0e53c441f
Sunil KS <kslvsunil@gmail.com>
2019-11-29 08:53:07 +0530

    changed variable names in encode-http

--- src/clj_http/core.clj
+++ src/clj_http/core.clj
@@ -1,34 +1,26 @@
 (ns clj-http.core
   (:import
-    [io.netty.channel
-     ChannelHandlerContext ChannelInboundHandlerAdapter ChannelFuture
-     ChannelInitializer ChannelHandler ChannelOption EventLoopGroup]
+    [io.netty.channel ChannelInboundHandlerAdapter ChannelInitializer ChannelHandler ChannelOption]
     [io.netty.channel.nio NioEventLoopGroup]
     [io.netty.buffer Unpooled]
-    [io.netty.channel.socket SocketChannel]
     [io.netty.channel.socket.nio NioServerSocketChannel]
     [io.netty.bootstrap ServerBootstrap]
     [io.netty.handler.codec.http QueryStringDecoder]
     [java.nio.charset Charset]
-    [java.time.format DateTimeFormatter]
-    [java.time ZonedDateTime ZoneOffset]
-    [java.io File ByteArrayOutputStream]
-    [java.nio.file Files]
-    [com.google.common.primitives Bytes])
-  (:require [clojure.string :refer [trim join]]
-            [clj-http.helper-macros :refer [cond-let]]
-            [clj-time.format :as f]
-            [clj-time.core :as t]
-            [pantomime.mime :refer [mime-type-of]]))
-(def ^:private time-format (f/formatter "EEE, dd MMM yyyy HH:mm:ss"))
-(defn- time->str [time] (str (f/unparse time-format time) " GMT"))
+    [java.io File]
+    [java.nio.file Files])
+  (:require
+    [clojure.string :refer [trim join]]
+    [clj-http.helper-macros :refer [cond-let]]
+    [clj-time.format :as f]
+    [clj-time.core :as t]
+    [pantomime.mime :refer [mime-type-of]]))
 
-(defn deep-merge [a b]
-  (merge-with (fn [x y]
-                (cond (map? y) (deep-merge x y)
-                      (vector? y) (concat x y)
-                      :else y))
-              a b))
+(def time-format (f/formatter "EEE, dd MMM yyyy HH:mm:ss"))
+(defn time->str [time] (str (f/unparse time-format time) " GMT"))
+(defn throw-error [s] (println s))
+
+(defn deep-merge [a b] (merge-with (fn [x y] (cond (map? y) (deep-merge x y) (vector? y) (concat x y) :else y)) a b))
 
 (defn handle-post [request-map])
 (defn handle-head [request-map])
@@ -38,23 +30,25 @@
 (defn handle-trace [request-map])
 (defn handle-connect [request-map])
 
-
-
 (defn handle-get [request-map]
   (try (let [raw-path (-> request-map :request-uri .path)
              abs-path (if (= "/" raw-path) (str "server-files/" "index.html") (str "server-files/" raw-path))
              resource (.. Files (readAllBytes (.toPath (File. abs-path))))]
-         {:body resource
+         {:response-body resource
           :headers {"Content-Type" (mime-type-of abs-path)
                     "Content-Length" (alength resource)}})
        (catch Exception e {:status-code 404
                            :reason-phrase "Not Found"})))
 
-(def http-methods {"GET" #(handle-get %) "POST" #(handle-post %) "HEAD" #(handle-head %)
-                   "OPTIONS" #(handle-options %) "PUT" #(handle-put %) "DELETE" #(handle-delete %)
-                   "TRACE" #(handle-trace %) "CONNECT" #(handle-connect %)})
-
-(defn throw-error [s] (println s))
+(def http-methods
+  {"GET" #(handle-get %)
+   "POST" #(handle-post %)
+   "HEAD" #(handle-head %)
+   "OPTIONS" #(handle-options %)
+   "PUT" #(handle-put %)
+   "DELETE" #(handle-delete %)
+   "TRACE" #(handle-trace %)
+   "CONNECT" #(handle-connect %)})
 
 (defn read-method [s]
   (if-let [method (re-find #"^\S+" s)]
@@ -90,12 +84,14 @@
     (throw-error "Invalid Msg body")))
 
 (defn encode-http-request [response-map ctx]
-  (let [status-line (str (response-map :protocol-version) " " (response-map :status-code) " " (response-map :reason-phrase) "\r\n")
-        headers-appended (str (reduce-kv (fn [s k v] (str s k ": " v "\r\n")) status-line (response-map :headers)) "\r\n")
-        s-h-bytes (.getBytes headers-appended (Charset/forName "UTF-8"))
-        concated-bytes (byte-array (mapcat seq [s-h-bytes (response-map :body)]))
-        bytes->byte-buf (.. Unpooled (wrappedBuffer concated-bytes))]
-    (.writeAndFlush ctx bytes->byte-buf)))
+  (let [st-ln (str (response-map :protocol-version) " "
+                   (response-map :status-code) " "
+                   (response-map :reason-phrase) "\r\n")
+        st-ln+hdr (str (reduce-kv (fn [s k v] (str s k ": " v "\r\n")) st-ln (response-map :headers)) "\r\n")
+        stln+hdr->bytes (.getBytes st-ln+hdr (Charset/forName "UTF-8"))
+        stln+hdr+body-bytes (byte-array (mapcat seq [stln+hdr->bytes (response-map :response-body)]))
+        full-bytes->byte-buf (.. Unpooled (wrappedBuffer stln+hdr+body-bytes))]
+    (.writeAndFlush ctx full-bytes->byte-buf)))
 
 (defn process-http-request [request-map]
   (let [method (request-map :request-method), handler (http-methods method)]
@@ -109,19 +105,18 @@
   (let [http-str (.toString msg (Charset/forName "UTF-8")), [method rmn] (read-method http-str),
         [uri rmn] (read-uri rmn), [version rmn] (read-version rmn), [headers rmn] (read-headers rmn),
         body (if-let [length (headers "content-length")] (read-body rmn (Integer/parseInt length)) nil)]
-    (assoc {} :request-method method :request-uri uri :protocol-version version :headers headers :body body)))
+    {:request-method method :request-uri uri :protocol-version version :headers headers :request-body body}))
 
 (defn handle-http-request []
   (proxy [ChannelInboundHandlerAdapter] []
     (channelRead [ctx msg]
-      (-> msg decode-http-request process-http-request (encode-http-request ctx)))
+      (-> msg (decode-http-request) (process-http-request) (encode-http-request ctx)))
     (exceptionCaught [ctx cause]
-      (do (.printStackTrace cause)
-          (.close ctx)))))
+      (do (.printStackTrace cause) (.close ctx)))))
 
-(defn bootstrap-server [boss-group worker-group handler]
+(defn bootstrap-server [group handler]
   (.. (ServerBootstrap.)
-      (group boss-group worker-group)
+      (group group)
       (channel NioServerSocketChannel)
       (childHandler
         (proxy [ChannelInitializer] []
@@ -133,12 +128,28 @@
       (childOption ChannelOption/SO_KEEPALIVE true)))
 
 (defn start-server [handler port]
-  (let [boss-group (NioEventLoopGroup.) worker-group (NioEventLoopGroup.)]
-    (try
-      (let [bootstrap (bootstrap-server boss-group worker-group handler)
-            channel (.. bootstrap (bind port) sync (channel))]
-        (-> channel .closeFuture .sync) channel)
-      (finally (do (.shutdownGracefully boss-group) (.shutdownGracefully worker-group))))))
+  (let [group (NioEventLoopGroup.)]
+   (try
+     (let [bootstrap (bootstrap-server group handler), channel (.. bootstrap (bind port) sync (channel))]
+       (-> channel .closeFuture .sync) channel)
+     (finally (.shutdownGracefully group)))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
